60cbec927fb584068f477d3c55b41b15debec815
Dominik Demaj-Seitz <12406359+domids@users.noreply.github.com>
2025-02-28 09:16:05 +0100

    add test cases

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -32,6 +32,45 @@ public class JPAUnitTestCase {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		// Do stuff...
+		entityManager.createQuery("select ke1_0 " +
+						"from Foo ke1_0 " +
+						"where ke1_0.tenantId = :tenant " +
+						"  and (lower(case " +
+						"                                                                when (ke1_0.contactType='company') then ( " +
+						"                                                                    coalesce((ke1_0.companyName1||''), '') || " +
+						"                                                                    coalesce(ke1_0.companyName2, '')) " +
+						"                                                                else ((coalesce((ke1_0.personLastName|| ''),  '') || " +
+						"                                                                       coalesce((ke1_0.personFirstName|| ''),  '')) || " +
+						"                                                                      coalesce(ke1_0.personPrefixTitle,  '')) end) like " +
+						"                                                      :searchTerm escape '!' or lower(((((case " +
+						"                                                                                           when (character_length(ke1_0.markCare) <> 0) " +
+						"                                                                                               then (''||coalesce(ke1_0.markCare, '')) " +
+						"                                                                                           else 'true' end || " +
+						"                                                                                       (ke1_0.street || " +
+						"                                                                                        coalesce(ke1_0.buildingNumber, ''))) || " +
+						"                                                                                      (ke1_0.postalCode || " +
+						"                                                                                       coalesce(ke1_0.city, ''))) || " +
+						"                                                                                     coalesce(ke1_0.postOfficeBox, '')) || " +
+						"                                                                                    (coalesce(ke1_0.countryIdentificationCode, '') || " +
+						"                                                                                     coalesce(ke1_0.countryName, '')))) like " +
+						"                                                                             :searchTerm escape '!' or lower(( " +
+						"                                                                              coalesce((ke1_0.line||''), '') )) like " +
+						"                                                                                             :searchTerm escape '!' or " +
+						"                                                                                                               lower(ke1_0.state) like " +
+						"                                                                                                               :searchTerm escape " +
+						"                                                                                                               '!' or " +
+						"                                                                                                               lower(ke1_0.externalNumber) like " +
+						"                                                                                                               :searchTerm escape " +
+						"                                                                                                               '!' or " +
+						"                                                                                                               lower(ke1_0.customerReferenceNumber) like " +
+						"                                                                                                               :searchTerm escape " +
+						"                                                                                                               '!' or " +
+						"                                                                                                               lower(ke1_0.baseAddressGLN) like " +
+						"                                                                                                               :searchTerm escape " +
+						"                                                                                                               '!') ")
+				.setParameter("tenant", "tenant")
+				.setParameter("searchTerm", "foo")
+				.getResultList();
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
